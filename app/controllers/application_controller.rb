72619ca55eaf8cf7dc26046cf2188e4ea6c6497c
madelinemc <madeline.coons@gmail.com>
2020-11-22 22:55:35 -0600

    commit tree a0e301ef1c54af991f264fd3b540dceedb94bf73

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,23 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(username: params[:username], password: params[:password])
 
+    if @user
+      session[:id] = @user.id
+      redirect to '/account'
+    end
+    redirect to '/error'
+    
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+      session.clear
+      redirect to '/'
   end
 
 
